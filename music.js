38d7991a0c28c215c528abb4a2d51374378249ca
Jeff Sisson <jeffsisson@gmail.com>
2012-11-10 14:53:10 -0500

    multi stuff

--- music.js
+++ music.js
@@ -45,22 +45,26 @@ var playlist = function(obj) {
 	this.position = 0;
 	this.successfullyTransferred = 0;
 	this.allTransfers = 0;
+	this.started = false;
 }
 playlist.prototype.play = function(position) {
-	console.log("Playing: "+this.list[position].readable());
+	this.log("Playing: "+this.list[position].readable());
 	var self = this,
 		player = new mpg()
 			.play(this.list[position].local)
-			.on('error',function(error) { console.log(error); })
+			.on('error',function(error) { 
+				self.log('error');
+				self.log(error); 
+			})
 			.on('end', function () {
 				player.close();
-				console.log('track ended')
+				self.log('track ended')
 				self.position++;
 				if (self.list[self.position]) {
-					console.log('next track')
+					self.log('next track')
 					self.play(self.position);
 				} else {
-					console.log('playlist finished!')
+					self.log('playlist finished!')
 					if (self.finish) self.finish();
 				}
 			});
@@ -72,27 +76,40 @@ playlist.prototype.onFinish = function(callback) {
 playlist.prototype.transferCheck = function( success ) {
 	if (success === true) this.successfullyTransferred++;
 	this.allTransfers++;
-	if (this.allTransfers == (this.list.length/2)) {
-		console.log('Transfers finished!');
+	// console.log('total'+this.list.length);
+	var midway = (this.list.length > 2) ? Math.round(this.list.length/2) : 1;
+	this.log("transferred so far "+this.allTransfers);
+	this.log('midway'+midway);
+	if (this.allTransfers >= midway && this.started === false) {
+		this.started = true;
+		this.log('Transfers finished!');
 		if (this.transferCallback) this.transferCallback();
 	}
 }
-playlist.prototype.transfer = function(callback) {
+playlist.prototype.transfer = function() {
 	var self = this;
-	this.transferCallback = callback;
 	this.list.forEach(function(t) {
 		t.transfer(function(success) { self.transferCheck(success); });
 	});
 }
-playlist.prototype.start = function(filename) {
+playlist.prototype.onTransfer = function(callback) {
+	if (this.started === true) {
+		callback();
+	} else {
+		this.transferCallback = callback;
+	}
+}
+playlist.prototype.log = function(msg) {
+	console.log(this.beginning.iso() + ": "+ msg );
+}
+playlist.prototype.start = function() {
 	var self = this;
-	this.transfer(function() {
+	this.onTransfer(function() {
 		self.play(self.position);
-	})
+	});
 	return this;
 }
 playlist.prototype.cleanup = function() {
-	console.log('cleaning up');
 	this.list.forEach(function(t) {
 		console.log(t.local);
 		fs.unlinkSync(t.local);
@@ -106,26 +123,45 @@ playlist.prototype.reverse = function() {
 }
 
 var music = {
-	current:{
-		list:[]
-	},
+	started:false,
 	playlist:function() {
-		return this.current;
+		return this.lists[0];
+	},
+	next : function() {
+		this.lists[0].start().log('starting');
+	},
+	add : function(data) {
+		var self = this,
+			p = new playlist(data);
+		p.transfer();
+		p.onFinish(function() {
+			p.cleanup();
+			p.log(self.lists.length);
+			self.lists.splice(0,1);
+			p.log(self.lists.length);
+			self.next(); // this restarts play
+			self.added--;
+			self.load();  // this restarts transfer
+		});
+		p.number = self.lists.push(p) - 1;
+		if (self.started !== true) {
+			self.next(); // first start happens here
+			self.started = true;
+		}
+		return p;
 	},
 	load:function() {
 		var self = this;
-		this.import(function(data) {
-			self.current = new playlist(data);
-			self.current.start().onFinish(function() {
-				self.current.cleanup();
-				console.log('starting over');
-				self.load();
+		while (this.added < 2) {
+			this.added++;
+			this.import(function(data) {
+				var p = self.add(data);
 			});
-		})
+		}
 	},
+	added:0,
 	lists:[],
 	import:function(callback) {
-		console.log("Reading in library");
 		var tracks = [],
 			startDate;
 		db.get("SELECT * FROM library ORDER BY RANDOM() LIMIT 1;",function(err,row) {
@@ -136,25 +172,25 @@ var music = {
 				if (!err) tracks.push(row);
 			},
 			function() {
-				console.log(tracks.length+" total tracks")
+				//console.log(tracks.length+" total tracks")
 				var added = [],
 					setDuration = 0,
 					maxInMinutes = config.length,
 					i = tracks.length-1;
-				console.log('Importing Library ...');
+				//console.log('Importing Library ... ' + startDate);
 
 				while ( ms2mn(setDuration) < maxInMinutes) {
 					var t = new track(tracks[ i ]);
 					if ( t.shouldAdd( startDate ) === true) {
-						console.log( t.readable() );
+						//console.log( t.readable() );
 						added.push(t);
 						setDuration += parseInt(t.duration);
 					}
 					i--;
 				}
 				var beginning = added[added.length-1].dateAdded;
-				console.log('Beginning: '+added[0].date()+' ending: '+added[added.length-1].date() );
-				console.log("Total: "+ms2mn(setDuration) + "m");
+				//console.log('Beginning: '+added[0].date()+' ending: '+added[added.length-1].date() );
+				//console.log("Total: "+ms2mn(setDuration) + "m");
 				callback({
 					list:added,
 					ending:startDate,
